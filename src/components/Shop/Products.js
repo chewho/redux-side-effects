d34b5c1620cbe6a390580bba420d920b74fd6465
chewho <molocrips@gmail.com>
2023-11-10 20:14:46 +0100

    feat: add logic for add/remove to/from cart, also change the quantity of the products

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,53 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "i1",
+    title: "Quantum Resonance Accelerator",
+    description: "Unleash the power of quantum resonance with this cutting-edge accelerator.",
+    price: 1999,
+  },
+  {
+    id: "i2",
+    title: "Eclipse Nebula Projector",
+    description: "Experience the wonders of the universe with this mesmerizing nebula projector.",
+    price: 149,
+  },
+  {
+    id: "i3",
+    title: "ChronoSphere Time Capsule",
+    description: "Capture moments and relive memories with this revolutionary time capsule.",
+    price: 299,
+  },
+  {
+    id: "i4",
+    title: "Phoenix Fire Elixir",
+    description: "Ignite your senses with the mythical properties of this ancient fire elixir.",
+    price: 49,
+  },
+  {
+    id: "i5",
+    title: "Dreamweaver's Reality Shifter",
+    description: "Shape your dreams with the extraordinary capabilities of this reality shifter.",
+    price: 129,
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
