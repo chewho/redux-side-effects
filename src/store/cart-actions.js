2e560fc7d6ba3540c48cb6cd65d9c64e515cdc4c
chewho <molocrips@gmail.com>
2023-11-15 18:17:25 +0100

    feat: fix some bugs, finalizing the fetching logic

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -5,7 +5,7 @@ export const fetchCartData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
       const response = await fetch(
-        "https://redux-f8b9a-default-rtdb.europe-west1.firebasedatabase.app//cart.json"
+        "https://redux-f8b9a-default-rtdb.europe-west1.firebasedatabase.app/cart.json"
       );
 
       if (!response.ok) {
@@ -19,7 +19,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -44,10 +49,10 @@ export const sendCartData = (cart) => {
 
     const sendRequest = async () => {
       const response = await fetch(
-        "https://redux-f8b9a-default-rtdb.europe-west1.firebasedatabase.app//cart.json",
+        "https://redux-f8b9a-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({ items: cart.items, totalQuantity: cart.totalQuantity }),
         }
       );
 
